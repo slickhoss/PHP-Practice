75ac34edac4c8c0a7c308ce306744a647d73436f
hoongandre <hoongandre@gmail.com>
2019-06-06 22:34:46 -0700

    Learning Outcomes - header redirects

---
+++ redirects.php
@@ -0,0 +1,11 @@
+<?php
+ header("Location: http://www.reddit.com/");//redirect to another website
+ //header("Location: sessions2.php"); //redirect to another page in directory
+  ?>
+
+<!DOCTYPE html>
+<html> 
+    <body>
+        
+    </body>
+</html>
